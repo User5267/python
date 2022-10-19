9fd75af0abd8b4aa744ad044cdda7debbc2cb1e0
User5267 <97493961+User5267@users.noreply.github.com>
2022-10-19 11:02:26 +0600

    Delete request1.py

--- request1.py
+++
@@ -1,8 +0,0 @@
-import requests
-url = "https://solana-gateway.moralis.io/nft/mainnet/2camF6sXuq4eQ2h2815xiA2r3BvoYNQbFztbLNymiwsq/metadata"
-headers = {
-    "accept": "application/json",
-    "X-API-Key": "z1SWBbYXT9C5u4QlvAAqxr6q2WIgJWbClpY9o2ESQfdSKOA4VRxTm9WoZmH2Zrnj"
-}
-response = requests.get(url, headers=headers)
-print(response.text)
